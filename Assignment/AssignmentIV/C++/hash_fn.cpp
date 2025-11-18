bb6ac099a4dbc4391147df65da0b467d3824e3b7
LightningXRay <rayh5487@gmail.com>
2025-11-18 23:55:27 +0800

    Update hash_fn.cpp

--- Assignment/AssignmentIV/C++/hash_fn.cpp
+++ Assignment/AssignmentIV/C++/hash_fn.cpp
@@ -9,17 +9,18 @@
    Development History:
     - 2025/11/11: Initial implementation
     - 2025/11/17: Refactored to use hash_fn.hpp
+    - 2025/11/18: add namespace delete hint
 
    Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
  */
 #include "hash_fn.hpp"
-
+using namespace std;
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
+   
     return key % m;  // basic division method
 }
 
-int myHashString(const std::string& str, int m) {
+int myHashString(const string& str, int m) {
     unsigned long hash = 0;
     // TODO: replace with your own design
     return static_cast<int>(hash % m);  // basic division method
