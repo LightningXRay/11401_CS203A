96d93986fb18d70ae493738b64414d32c4d03a6d
LightningXRay <rayh5487@gmail.com>
2025-11-20 23:57:53 +0800

    Update hash_fn.cpp

--- Assignment/AssignmentIV/C++/hash_fn.cpp
+++ Assignment/AssignmentIV/C++/hash_fn.cpp
@@ -19,7 +19,7 @@
 #include "hash_fn.hpp"
 using namespace std;
 int myHashInt(int key, int m) {
-    int k=1,n=1;//k:temp
+    int k=1;//k:temp
     while(key!=1){//k+key+key/2+key/4...+2
         k+=key;
         key/=2;
